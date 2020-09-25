453ce07b35f7c4fff5f6f7bbef2c3d12a2774604
yezii <1126yezy@gmail.com>
2020-09-26 01:06:36 +0900

    Edit tiny things

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -30,7 +30,7 @@ const Loading = styled.div`
 
 const Movies = styled.div`
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: repeat(4, 1fr);
   grid-gap: 25px;
   width: 100%;
   position: relative;
